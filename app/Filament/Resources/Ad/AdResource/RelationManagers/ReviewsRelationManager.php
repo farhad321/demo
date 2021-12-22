b8f10690a70e6ce6b43db4af46d6c3069131208e
farhad <farhad.3.rohani@gmail.com>
2021-12-23 00:09:06 +0330

    ad review

--- app/Filament/Resources/Ad/AdResource/RelationManagers/ReviewsRelationManager.php
+++ app/Filament/Resources/Ad/AdResource/RelationManagers/ReviewsRelationManager.php
@@ -27,50 +27,51 @@ class ReviewsRelationManager extends HasManyRelationManager
 
  public static function form(Form $form): Form
  {
-  return $form->schema([
-                        Toggle::make('is_visible')
-                              ->inline(false),
-                        TextInput::make('title')
-                                 ->required(),
-                        Card::make()
-                            ->schema([
-                                      RichEditor::make('content')
-                                                ->disableToolbarButtons([
-                                                                         'attachFiles',
-                                                                         'codeBlock',
-                                                                        ])
-                                     ])
-                            ->columnSpan(3),
-                        BelongsToSelect::make('user_id')
-                                       ->visible(fn(?Review $record): bool => $record?->user ? false : true)
-                                       ->relationship('user', 'name',
-                                        fn(Builder $query) => $query->where('rule', 'admin'))
-                                       ->default(function () {
-                                        return User::where('rule', 'admin')
-                                                   ->first()->id;
-                                       }),
-                        Placeholder::make('user_id')
-                                   ->label('Created By')
-                                   ->content(fn(?Review $record): string => $record ? $record->user?->name : '-')
-                                   ->visible(fn(?Review $record): bool => $record?->user ? true : false),
-                       ]);
+  $schema = [
+   Toggle::make('is_visible')
+         ->inline(false),
+   TextInput::make('title')
+            ->required(),
+   Card::make()
+       ->schema([
+                 RichEditor::make('content')
+                           ->disableToolbarButtons([
+                                                    'attachFiles',
+                                                    'codeBlock',
+                                                   ])
+                ])
+       ->columnSpan(3),
+   BelongsToSelect::make('user_id')
+                  ->visible(fn(?Review $record): bool => $record?->user ? false : true)
+                  ->relationship('user', 'name', fn(Builder $query) => $query->where('rule', 'admin'))
+                  ->default(function () {
+                   return User::where('rule', 'admin')
+                              ->first()->id;
+                  }),
+   Placeholder::make('user_id')
+              ->label('Created By')
+              ->content(fn(?Review $record): string => $record ? $record->user?->name : '-')
+              ->visible(fn(?Review $record): bool => $record?->user ? true : false),
+  ];
+  return $form->schema($schema);
  }
 
  public static function table(Table $table): Table
  {
-  return $table->columns([
-                          TextColumn::make('title')
-                                    ->searchable()
-                                    ->sortable(),
-                          BooleanColumn::make('is_visible')
-                                       ->trueIcon('heroicon-o-badge-check')
-                                       ->falseIcon('heroicon-o-x-circle')
-                                       ->sortable(),
-                          TextColumn::make('user.name')
-                                    ->label('Created By')
-                                    ->searchable()
-                                    ->sortable(),
-                         ])
+  $columns = [
+   TextColumn::make('title')
+             ->searchable()
+             ->sortable(),
+   BooleanColumn::make('is_visible')
+                ->trueIcon('heroicon-o-badge-check')
+                ->falseIcon('heroicon-o-x-circle')
+                ->sortable(),
+   TextColumn::make('user.name')
+             ->label('Created By')
+             ->searchable()
+             ->sortable(),
+  ];
+  return $table->columns($columns)
                ->filters([//
                          ]);
  }
